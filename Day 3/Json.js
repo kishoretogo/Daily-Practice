69f32db749cc647eef4dcb27466e55a62c9aaa55
kishoreKB98 <105081740+kishoreKB98@users.noreply.github.com>
2022-05-31 08:28:14 +0530

    Add files via upload

---
+++ Day 3/Json.js
@@ -0,0 +1,53 @@
+{
+    "General": {
+      "name": "Kishore",
+      "email": "kokishore@gmail.com",
+      "phone": "995234134",
+      "degree": "BE Aeronautical",
+      "website": "www.kishore.com",
+      
+
+    "address": {
+        "street address": "1/2,kamarajapuram",
+        "postalCode": "643425",
+        "city": "karur",
+        "countryCode": "Tamilnadu",
+      },
+     }
+     },
+    "work": [
+      {
+        "company": " hello am",
+        "position": " 3d visualiser",,
+        "summary": "egar to create 3d objects": 
+       }
+    ],
+    
+    "education": [
+      {
+        "institution": "Kumaraguru  college of technology",
+        "course": " BE Aeronautical",
+        "startDate": " 12/06/2016",
+        "endDate": " 24/03/2020",
+        "gpa": "7.8",
+         }
+    ],
+    "awards": [
+      {
+        "Award": "Achiever award",
+        "summary": "for the contribution towards arts and photography"
+      }
+    ],
+    "skills": [
+      {
+        "name": "[photography, ar vr, graphic design, fill stack developer]",
+        
+      }
+    ],
+    "languages": [
+      {
+        "language": "English",
+        "Proficiency": "Full Proffesional Proficiency"
+      }
+    ],
+    
